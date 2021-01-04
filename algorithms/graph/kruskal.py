8006c60d4968ee4288824d62ad0f8a94f6431b23
CoodingPenguin <unodostre@naver.com>
2021-01-04 22:42:34 +0900

    study: ✍ add kruskal algorithm

--- algorithms/graph/kruskal.py
+++ algorithms/graph/kruskal.py
@@ -1,7 +1,6 @@
-# title: 서로소 집합 알고리즘으로 그래프 사이클 판별
-# src: 이것이 취업을 위한 코딩테스트다 p.279, 280
-# time: 개선 전 - O(VM) / 개선 후 - O(V + MlogV) (V=노드개수, M=연산개수)
-
+# title: 최소 신장 트리 알고리즘 중 하나인 크루스칼 알고리즘
+# src: 이것이 취업을 위한 코딩테스트다 p.288, 289
+# time: O(ElogE) (E=간선의 개수)
 
 def find_parent(parent, x):
     '''
@@ -32,19 +31,22 @@ def union_parent(parent, a, b):
 if __name__ == "__main__":
     v, e = map(int, input().split())    # 노드와 간선의 개수
     parent = [0] + list(range(1, v+1))  # 부모 테이블
-    cycle = False                       # 사이클 발생 여부
+    edges = []      # 모든 간선을 담을 리스트
+    result = 0      # 최종 비용                    
 
-    for i in range(e):
-        a, b = map(int, input().split())
-        # 사이클이 발생한 경우 종료
-        if find_parent(parent, a) == find_parent(parent, b):
-            cycle = True
-            break
-        # 사이클이 발생하지 않았따면 Union 연산 수행
-        else:
+    for _ in range(e):
+        a, b, cost = map(int, input().split())
+        # 비용순으로 정렬하기 위해 비용을 가장 앞으로 설정
+        edges.append((cost, a, b))
+
+    # 간선을 비용순으로 정렬
+    edges.sort()
+
+    for edge in edges:
+        cost, a, b = edge
+        # 간선을 확인하며 사이클이 발생하지 않는 경우만 집합에 포함
+        if find_parent(parent, a) != find_parent(parent, b):
             union_parent(parent, a, b)
-
-    if cycle:
-        print('사이클이 발생했습니다.')
-    else:
-        print('사이클이 발생하지 않았습니다.')
+            result += cost
+    
+    print(result)
