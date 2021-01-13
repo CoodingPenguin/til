d02402b5036c44a5da982d5c506caa3e112fb2ee
CoodingPenguin <unodostre@naver.com>
2021-01-14 00:37:04 +0900

    study: ✍ add rotate90 code

--- algorithms/math/rotate_matrix_by_90.py
+++ algorithms/math/rotate_matrix_by_90.py
@@ -1,7 +1,8 @@
 # title: 시계방향으로 행렬을 90도 회전
 # src: 이것이 취업을 위한 코딩테스트다 p. 520
+# src: https://github.com/tjdud0123/daily_algorithm#-90도-회전
 
-def rotate_a_matrix_by_90_degree(matrix):
+def rotate_90_with_loop(matrix):
     '''
     행렬을 90도 회전한다
     - `matrix`: n*m으로 이루어진 행렬 (n과 m은 양의 정수)
@@ -16,10 +17,22 @@ def rotate_a_matrix_by_90_degree(matrix):
     
     return result
 
+def rotate_90_with_zip(matrix):
+    '''
+    행렬을 90도로 회전한다
+     - `matrix`: n*m으로 이루어진 행렬 (n과 m은 양의 정수)
+    '''
+    # 1. 행렬의 행을 뒤집는다. 
+    # 2. 각 행의 요소를 가져와 하나의 행으로 만든다.
+    return list(zip(*matrix[::-1]))
 
 if __name__ == '__main__':
     before = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    after = rotate_a_matrix_by_90_degree(before)
+    after1 = rotate_90_with_loop(before)
+    after2 = rotate_90_with_zip(before)
 
     for i in range(len(before)):
-        print(after[i])
+        print(after1[i])
+    print()
+    for i in range(len(before)):
+        print(after2[i])
